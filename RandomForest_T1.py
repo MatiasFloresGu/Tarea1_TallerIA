e959a67d18b008f0eebc2d7b306cafb0b9d93ff0
MatiasFloresGu <mafloresgu@gmail.com>
2025-04-05 21:14:42 -0300

    RandomForest_v2

--- RandomForest_T1.py
+++ RandomForest_T1.py
@@ -1,8 +1,31 @@
-import numpy as np
-import pandas as pd
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import load_digits
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score
 
+# Cargar el dataset de dígitos
+digits = load_digits()
+
+
+X = digits.data  
+y = digits.target
+
+# Dividir los datos en dos conjuntos
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Configuracion random Forest
+clf = RandomForestClassifier(n_estimators=100, random_state=42)
+
+# Entrenar el modelo
+clf.fit(X_train, y_train)
+
+# Predecir en el conjunto de prueba
+y_pred = clf.predict(X_test)
+
+# Evaluar el modelo
+accuracy = accuracy_score(y_test, y_pred)
+
+
+print(f"Exactitud (accuracy) del modelo: {accuracy * 100:.2f}%")
+print("\nInforme de clasificación:")
+print(classification_report(y_test, y_pred))
